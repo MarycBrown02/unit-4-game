af4a880e725f441befa9d6dce38cdf8cffa0aa7a
MarycBrown02 <marycbrown01@sbcgloba.net>
2018-09-15 00:19:47 -0500

    modified 2 files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 
+    var num = Math.floor(Math.random() * 102 + 19);
+    $('#scoreToMatch').text(num);
     
-    })
+    var greenGem = Math.floor(Math.random() * 12 + 1); 
+    var blueGem = Math.floor(Math.random() * 12 + 1); 
+    var redGem = Math.floor(Math.random() * 12 + 1); 
+    var purpleGem = Math.floor(Math.random() * 12 + 1); 
+    
+    var win = 0
+    var loose = 0
+    var total = 0
+
+    
+
+});
